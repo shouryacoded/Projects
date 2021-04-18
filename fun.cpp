e81419e6cde708f21457b8551bb6b2f60b16660d
shouryacoded <shourya.onine23@gmail.com>
2021-04-18 12:47:47 +0530

    Fun.cpp commit

--- fun.cpp
+++ fun.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-/*void strLen(string Fname, string Lname)
+void strLen(string Fname, string Lname)
 {
     int m = Fname.length();
     int n = Lname.length();
@@ -36,8 +36,8 @@ int main()
     cout << "The lagest number is : " << disp(ar) << endl;
     return 0;
 }
-*/
-int fac(int n)
+
+/*int fac(int n)
 {
     if(n==1)
     return 1;
@@ -52,4 +52,4 @@ int main()
     cin >> num;
     cout << "The factorial of " << num << " is :  " <<fac(num) << endl;
     return 0;
-}
+}*/
