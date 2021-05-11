18b6ab8c84ff44765640c3c7c809e84faf2c5c1e
shouryacoded <shourya.online23@gmail.com>
2021-05-11 08:42:00 +0530

    Mantain

--- class_test.cpp
+++ class_test.cpp
@@ -3,6 +3,11 @@ using namespace std;
 class best{
     string name;
     public:
+    int roll;
+    best(int aroll)
+    {
+        roll = aroll;
+    }
     best(string Aname)
     {
         setbest(Aname);
@@ -15,11 +20,18 @@ class best{
     {
         return name;
     }
+    int getRoll()
+    {
+        return roll;
+    }
 };
 int main()
 {
     system("clear");
-    best stud("Shourya");
+    best stud(12);
+    stud("Shourya");
+//    stud("Shourya");  
     cout << "The name entered is : " << stud.getbest() << endl;
+    cout << "The roll numbers passed to the constructor is : " << stud.roll;
     return 0;
 }
